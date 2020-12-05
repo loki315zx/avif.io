a8f456dd028b341e756beaf198a5e20029dfcd27
ADESSO\juschmitz <justin.schmitz@adesso.de>
2020-12-05 22:28:15 +0100

    further changes

--- components/App.tsx
+++ components/App.tsx
@@ -3,13 +3,13 @@ import ReactCompareImage from "react-compare-image";
 import _ from "lodash";
 import Conversion from "./Conversion";
 import Dropzone from "./Dropzone";
-import Tutorials from "./Tutorials";
 import DownloadAllButton from "./DownloadAllButton";
 import SettingsBox, { Settings } from "./SettingsBox";
 import Converter from "../src/converter";
 import { FileWithId, uniqueId } from "../src/utils";
 import lion from "../assets/images/lion.jpg";
 import lion2 from "../assets/images/lion2.avif";
+import TutorialsBox from "./TutorialsBox";
 
 export default function App() {
   const [converter, setConverter] = useState<Converter>();
@@ -91,24 +91,53 @@ export default function App() {
         <div className="chevron" />
       </div>
 
-      <Tutorials />
+      <div className="tutorial_container">
+      <div className="text-60 f3 white">
+        How to use avif in a productive environment.
+      </div>
+      <div className="text-40 white s5">
+        AV1 (.avif) is the solution. It is developed by the Alliance for Open
+        Media in collaboration with Google, Mozilla, Intel and other tech
+        giants.
+      </div>
+      <div className="tutorials_container s5">
+        <TutorialsBox cssclass="css" title="CSS" />
+        <TutorialsBox cssclass="html" title="HTML" />
+        <TutorialsBox cssclass="js" title="JS" />
+        <TutorialsBox cssclass="svelte" title="Svelte" />
+        <TutorialsBox cssclass="react" title="React" />
+        <TutorialsBox cssclass="angular" title="Angular" />
+        <TutorialsBox cssclass="vue" title="Vue" />
+        <TutorialsBox cssclass="firefox" title="Firefox" />
+        <TutorialsBox cssclass="gimp" title="GIMP" />
+        <TutorialsBox cssclass="windows" title="Windows" />
+        <TutorialsBox cssclass="netlify" title="Netlify" />
+        <TutorialsBox cssclass="wordpress" title="WordPress" />
+      </div>
+    </div>
       <section id="whatisavif">
-        <div className="badge__grid">
-          <div className="white center s2 badge__avif">
-            <p className="bold">
-              What is<span>.avif?</span>
+
+          <div className="white center s3 badge__avif">
+            <p className="bold">.AVIF
             </p>
           </div>
-          <div className="badge__grid__item">80% more bullshit</div>
-          <div className="badge__grid__item">242 more cool things</div>
+          <div className="badge__grid">
+          <div className="badge__grid__item">usually reduces image file size by 20-90%</div>
+          <div className="badge__grid__item">reduces required bandwidth for service providers</div>
           <div className="badge__grid__item">
             developed by tech giants like Google, Apple, Facebook, Microsoft,
             Mozilla, Netflix, Nvidia
           </div>
-          <div className="badge__grid__item">80% more bullshit</div>
-          <div className="badge__grid__item">242 more cool things</div>
+          <div className="badge__grid__item">open and royalty-free for everyone</div>
+          <div className="badge__grid__item">one of the highest quality to compression rates ever achieved</div>
           <div className="badge__grid__item">
-            developed by Google, Apple and more
+            already supported by Chrome, Opera and Firefox
+          </div>
+          <div className="badge__grid__item">
+            supports transparency
+          </div>
+          <div className="badge__grid__item">
+            supports animated frames
           </div>
         </div>
       </section>
