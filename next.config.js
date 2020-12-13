b11488e0e1536cb97f9cf07a31709ac59fd7cc0c
justinschmitz97 <justin.schmitz97@gmail.com>
2020-12-13 18:03:01 +0100

    image optimization automation

--- next.config.js
+++ next.config.js
@@ -1,2 +1,14 @@
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
 const withImages = require("next-images");
-module.exports = withImages();
+
+module.exports = withPlugins([
+  [
+    optimizedImages,
+    {
+      optimizeImagesInDev: "true",
+    },
+  ],
+
+  withImages(),
+]);
