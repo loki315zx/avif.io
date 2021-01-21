8c171d3bc3dd7e6d921aa8f94516a349ba4c528a
Justin Schmitz <justin.schmitz97@gmail.com>
2021-01-21 15:23:45 +0100

    fix

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -32,7 +32,7 @@ export default function Meta(props: MetaProps) {
       <meta property="og:url" content={`https://avif.io/${props.url}`} />
       <meta property="og:title" content={"avif.io" + " | " + props.title} />
       <meta property="og:description" content={props.description} />
-      <meta property="og:image" content={props.image} />
+      <meta property="og:image" content={"https://avif.io" + props.image} />
       <meta property="og:type" content="article" />
       {props.blog ? (
         <script
