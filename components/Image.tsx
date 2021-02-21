a1153cc996d8b6b7e0a42672af56262b4b1566b0
Justin Schmitz <justin.schmitz97@gmail.com>
2021-02-21 16:17:36 +0100

    update

--- components/Image.tsx
+++ components/Image.tsx
@@ -4,17 +4,17 @@ export default function Image(props: { alt: string; url: string }) {
       <picture>
         <source
           sizes="(max-width: 720px) 100vw, 720px"
-          srcSet={`/img/${props.url}-1440.avif 2x, /img/${props.url}-720.avif 720w, /img/${props.url}-630.avif 630w, /img/${props.url}-540.avif 540w, /img/${props.url}-450.avif 450w, /img/${props.url}-360.avif 360w`}
+          srcSet={`/img/${props.url}-1440.avif 1440w, /img/${props.url}-720.avif 720w, /img/${props.url}-630.avif 630w, /img/${props.url}-540.avif 540w, /img/${props.url}-450.avif 450w, /img/${props.url}-360.avif 360w`}
           type="image/avif"
         />
         <source
           sizes="(max-width: 720px) 100vw, 720px"
-          srcSet={`/img/${props.url}-1440.webp 2x, /img/${props.url}-720.webp 720w, /img/${props.url}-630.webp 630w, /img/${props.url}-540.webp 540w, /img/${props.url}-450.webp 450w, /img/${props.url}-360.webp 360w`}
+          srcSet={`/img/${props.url}-1440.webp 1440w, /img/${props.url}-720.webp 720w, /img/${props.url}-630.webp 630w, /img/${props.url}-540.webp 540w, /img/${props.url}-450.webp 450w, /img/${props.url}-360.webp 360w`}
           type="image/webp"
         />
         <source
           sizes="(max-width: 720px) 100vw, 720px"
-          srcSet={`/img/${props.url}-1440.jpg 2x, /img/${props.url}-720.jpg 720w, /img/${props.url}-630.jpg 630w, /img/${props.url}-540.jpg 540w, /img/${props.url}-450.jpg 450w, /img/${props.url}-360.jpg 360w`}
+          srcSet={`/img/${props.url}-1440.jpg 1440w, /img/${props.url}-720.jpg 720w, /img/${props.url}-630.jpg 630w, /img/${props.url}-540.jpg 540w, /img/${props.url}-450.jpg 450w, /img/${props.url}-360.jpg 360w`}
           type="image/jpeg"
         />
         <img
@@ -22,7 +22,7 @@ export default function Image(props: { alt: string; url: string }) {
           height="1620"
           decoding="async"
           loading="lazy"
-          src={`/img/${props.url}-720.jpg`}
+          src={`/img/${props.url}-1440.jpg`}
           alt={props.alt}
         />
       </picture>
