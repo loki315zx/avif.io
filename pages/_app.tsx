34f031a79fedf683de9d44a88f406e46ff367ced
Justin Schmitz <justin.schmitz97@gmail.com>
2021-02-21 16:24:54 +0100

    small fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -34,10 +34,10 @@ export default function AvifIo({ Component, pageProps }: any) {
       <Footer />
       <Cookies />
       <SocialShare />
-      <script src="/__/firebase/8.2.6/firebase-app.js"></script>
-      <script src="/__/firebase/8.2.6/firebase-analytics.js"></script>
-      <script src="/__/firebase/8.2.6/firebase-database.js"></script>
-      <script src="/__/firebase/8.2.6/firebase-storage.js"></script>
+      <script src="/__/firebase/8.2.8/firebase-app.js"></script>
+      <script src="/__/firebase/8.2.8/firebase-analytics.js"></script>
+      <script src="/__/firebase/8.2.8/firebase-database.js"></script>
+      <script src="/__/firebase/8.2.8/firebase-storage.js"></script>
       <script src="/__/firebase/init.js"></script>
       <script src="console-hack.js"></script>
       <script>firebase.analytics()</script>
