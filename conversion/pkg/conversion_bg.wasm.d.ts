74e5b26b3ab637c81bbb59623a100ba744e642d7
Justin Schmitz <justin.schmitz97@gmail.com>
2021-05-09 23:52:31 +0200

    package update

--- conversion/pkg/conversion_bg.wasm.d.ts
+++ conversion/pkg/conversion_bg.wasm.d.ts
@@ -11,7 +11,14 @@ export function __wbg_set_conversionresult_error(a: number, b: number): void;
 export function __wbg_get_conversionresult_error_size(a: number): number;
 export function __wbg_set_conversionresult_error_size(a: number, b: number): void;
 export function convert_to_avif(a: number, b: number, c: number, d: number): number;
-export function rgba_to_avif(a: number, b: number, c: number, d: number, e: number, f: number): number;
+export function rgba_to_avif(
+  a: number,
+  b: number,
+  c: number,
+  d: number,
+  e: number,
+  f: number
+): number;
 export function __wbg_conversionoptions_free(a: number): void;
 export function __wbg_get_conversionoptions_effort(a: number): number;
 export function __wbg_set_conversionoptions_effort(a: number, b: number): void;
