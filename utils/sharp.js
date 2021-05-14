4db35b645cf7c3c3b39a947788259c5051d74b04
Justin Schmitz <justin.schmitz97@gmail.com>
2021-05-14 11:24:39 +0200

    .

--- utils/sharp.js
+++ utils/sharp.js
@@ -4,9 +4,9 @@ const path = require("path");
 
 const output = "../public/img/";
 const input = "../images/";
-const jpgQuality = { mozjpeg: true, quality: 60, progressive: true };
-const webpQuality = { quality: 60, reductionEffort: 6 };
-const avifQuality = { quality: 50, speed: 1 };
+const jpgQuality = { mozjpeg: true, quality: 50, progressive: true };
+const webpQuality = { quality: 50, reductionEffort: 6 };
+const avifQuality = { quality: 45, speed: 1 };
 const sizes = [1440, 720, 540, 360];
 
 fs.readdir(input, (err, files) => {
