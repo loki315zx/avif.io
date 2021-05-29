42dfe4c38a290ccf8dd848965fdce4d8070877c1
ADESSO\juschmitz <justin.schmitz@adesso.de>
2021-05-30 00:45:28 +0200

    small fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 const withPlugins = require("next-compose-plugins");
 const images = require("next-images");
+const bundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
-module.exports = withPlugins([images]);
+const baseUrl = "";
+
+module.exports = withPlugins([
+  images,
+  [
+    bundleAnalyzer,
+    {
+      trailingSlash: true,
+      basePath: baseUrl,
+      env: {
+        baseUrl: baseUrl,
+      },
+    },
+  ],
+]);
