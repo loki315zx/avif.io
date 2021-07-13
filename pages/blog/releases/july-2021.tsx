a1638f10fa672064952401a8cef419a9a8e0b32b
Justin Schmitz <justin.schmitz97@gmail.com>
2021-07-13 16:01:07 +0200

    Rewritten July Release + Added Git Templates to ignore

--- pages/blog/releases/july-2021.tsx
+++ pages/blog/releases/july-2021.tsx
@@ -25,19 +25,20 @@ export default function BlogPost() {
         contentTableCallback={contentTableCallback}
         text="We are open source"
       />
-      With every project comes responsibilities. As a team of two people being
-      involved with our daily jobs and family, we couldn't make up the time to
-      keep the development as steady as we wished we could. Realizing that this
-      project is in a fast-living environment, we figured it would be the best
-      idea to enable people the ability to create their own versions and work on
-      their own features. We have been asked multiple times to release the
-      source code. We have been asked many times how people can help us with the
-      development. We're happy to announce that by today, we are an open-source
-      project. Not only does this help the development, but also people can
-      easily understand and get insights on how and what data we process. As
-      file converters usually tend to save your images in a cloud, being
-      open-source allows us to give people the confidence that their images are
-      secure. In order to achieve a better cooperation, we have
+      Each project comes with a set of responsibilities. As a two-person team
+      involved in our day-to-day jobs and family, we could not dedicate the time
+      necessary to keep the development as steady as we would have liked. Due to
+      the fast-paced nature of the project, we thought it would be best to
+      provide people with the ability to create their own versions and work on
+      their own features. Multiple requests have been made for the source code
+      to be released. Many people have asked how they can assist us with the
+      development. <br />
+      We are pleased to announce that we are now an open-source project. Besides
+      making the development process easier, the general public is also able to
+      gain insight into how and what data we process. Due to the fact that file
+      converters typically save your images in the cloud, being open-source
+      provides people with the confidence that their images are secure. As a
+      means of achieving a better cooperative relationship, we have
       <Checklist
         items={[
           "upgraded all dependencies to their latest version",
