d7d835b5f1abe38c8cb1122541aa0b2032f1bd82
Justin Schmitz <justin.schmitz97@gmail.com>
2021-07-18 19:00:47 +0200

    Add JPEGXL comparison article

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -2,7 +2,12 @@ import Posts from "@components/Blog/Posts";
 import Layout from "@components/Layout";
 import meta from "@lib/meta.json";
 
-const comparisons = [meta.avifVsJpg, meta.avifVsPng, meta.avifVsGif];
+const comparisons = [
+  meta.avifVsJpg,
+  meta.avifVsPng,
+  meta.avifVsGif,
+  meta.avifVsJpegxl,
+];
 const releases = [
   meta.january2021,
   meta.february2021,
